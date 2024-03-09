75d215c94abcf21b1fc2dd357ed56771e5acb44d
cryscan <cryscan@umich.edu>
2024-03-10 01:54:26 +0800

    Add `codebase.md`

--- src/main.rs
+++ src/main.rs
@@ -529,37 +529,6 @@ async fn model_route(receiver: Receiver<ThreadRequest>) -> Result<()> {
     }
 }
 
-pub async fn try_request_info(sender: Sender<ThreadRequest>) -> Result<RuntimeInfo> {
-    let (info_sender, info_receiver) = flume::unbounded();
-    let _ = sender.send(ThreadRequest::Info(info_sender));
-    let info = info_receiver.recv_async().await?;
-    Ok(info)
-}
-
-pub async fn request_info(sender: Sender<ThreadRequest>, sleep: Duration) -> RuntimeInfo {
-    loop {
-        if let Ok(info) = try_request_info(sender.clone()).await {
-            break info;
-        }
-        tokio::time::sleep(sleep).await;
-    }
-}
-
-pub async fn request_info_stream(
-    sender: Sender<ThreadRequest>,
-    info_sender: Sender<RuntimeInfo>,
-    sleep: Duration,
-) {
-    loop {
-        if let Ok(info) = try_request_info(sender.clone()).await {
-            if info_sender.send(info).is_err() {
-                break;
-            }
-        }
-        tokio::time::sleep(sleep).await;
-    }
-}
-
 #[derive(Parser, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
 struct Args {
