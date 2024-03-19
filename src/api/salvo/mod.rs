0cc157ab1330f8cdb43ebe2918fc8ea514f578d4
Long Zou <zouyunlong@siline.cn>
2024-03-19 10:33:26 +0800

    update

--- src/api/salvo/mod.rs
+++ src/api/salvo/mod.rs
@@ -1,13 +1,7 @@
-
-
-
-
-
 pub mod adapter;
 pub mod file;
 pub mod load;
 pub mod oai;
-
 
 pub use file::{salvo_dir, salvo_load_config, salvo_models, salvo_save_config, salvo_unzip};
 pub use load::{salvo_info, salvo_load, salvo_state, salvo_unload};
