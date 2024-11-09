316ae85a740074b5f7323e290b432563d887ee14
研究社交 <cryscan@umich.edu>
2024-11-10 01:02:10 +0800

    Update dependencies and add icon. (#146)

---
+++ crates/ai00-server/build.rs
@@ -0,0 +1,13 @@
+use {
+    std::{env, io},
+    winresource::WindowsResource,
+};
+
+fn main() -> io::Result<()> {
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        WindowsResource::new()
+            .set_icon("../../assets/icon/ai00.ico")
+            .compile()?;
+    }
+    Ok(())
+}
